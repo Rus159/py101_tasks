67ab92ad47afb6aacef64cc179afa69ed7b481f8
Rus159 <zabelin.tollyatti@yandex.ru>
2020-10-30 20:02:40 +0400

    done

--- guess_num.py
+++ guess_num.py
@@ -13,6 +13,25 @@
 
 Тебе может понадобится модуль random, цикл while и ветвления
 """
+import random
+
 
 if __name__ == '__main__':
-    pass
+    num = random.randint(0, 1000000)
+
+    print('С подключением, %username%! Попробуй угадать число от 0 до 1 000 000. Для выхода из игры введи exit или пустую строку.')
+    answer = '0'
+    while int(answer) != num and answer != 'exit' and answer != '':
+        answer = input('Введи предполагаемое число: ')
+        if answer == '':
+            print('Не введено число')
+        if answer == 'exit':
+            pass
+        elif 1 > int(answer) > 1000000:
+            print('Число не соответствует диапазону')
+        elif int(answer) == num:
+            print('Число угадано!')
+        elif num > int(answer):
+            print('Число меньше загаданного')
+        else:
+            print('Число больше загаданного')
